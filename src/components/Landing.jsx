14dce725180379d4f9780e5af2375263e722268a
dwivedi-ritik <dwi.ritik20@gmail.com>
2021-09-06 16:26:54 +0530

    fixed footer

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -19,19 +19,13 @@ const Landing = () => {
             <footer className="text-gray-600 body-font">
                 <div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
                     <button className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-10 h-10 text-white p-2 bg-yellow-500 rounded-full" viewBox="0 0 24 24">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-8 h-8 text-white p-2 bg-yellow-500 rounded-full" viewBox="0 0 24 24">
                             <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
                         </svg>
-                        <span className="ml-3 text-xl">Yts Clone</span>
+                        <span className="ml-3 text-sm  sm:text-lg font-bold">YTS Clone</span>
                     </button>
-                    <p className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">© 2020 YTS
-                        <a href="https://twitter.com/knyttneve" className="text-gray-600 ml-1" rel="noopener noreferrer" target="_blank">@notorious</a>
-                    </p>
                     <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
-                        <a className="text-gray-500 font-bold" target="_blank" rel="noreferrer" href="https://github.com/dwivedi-ritik/yts/tree/deploy">
-                            {/* <svg fill="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
-                                <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
-                            </svg> */}
+                        <a className="text-gray-500 text-mid font-bold" target="_blank" rel="noreferrer" href="https://github.com/dwivedi-ritik/yts/tree/deploy">
                             Github
                         </a>
                     </span>
