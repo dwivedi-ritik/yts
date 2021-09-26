fd639504ecc63ddefee2f7870e804bea7e7a892f
dwivedi-ritik <dwi.ritik20@gmail.com>
2021-09-26 21:17:42 +0530

    fixed api issue

--- src/components/SearchComp.jsx
+++ src/components/SearchComp.jsx
@@ -45,7 +45,7 @@ class SearchComp extends React.Component {
             const res = await axios(obj)
             const data = await res.data
             console.log(data)
-            if (data.data.movie_count !== 0)
+            if (data.data.movie_count !== 0 && data.data.movies)
                 return data.data.movies
             else {
                 return null
