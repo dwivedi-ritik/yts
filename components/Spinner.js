7e7ea184e3cf7b0e31ad72b437f3b5d6a3142f90
dwivedi-ritik <dwi.ritik20@gmail.com>
2022-07-28 15:50:15 +0530

    build

--- components/Spinner.js
+++ components/Spinner.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Spinner = () => {
     return (
-        <div class="relative flex justify-center items-center h-20">
-            <div class="rounded animate-spin ease duration-300 w-8 h-8 border-2 border-yellow-500"></div>
+        <div className="relative flex justify-center items-center h-20">
+            <div className="rounded animate-spin ease duration-300 w-8 h-8 border-2 border-yellow-500"></div>
         </div>
     )
 }
