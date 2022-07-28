a6af35e4fd3e8a5f0a69d71055d2a707fbb44926
dwivedi-ritik <dwi.ritik20@gmail.com>
2022-07-28 15:48:38 +0530

    build

--- components/SearchComp.js
+++ components/SearchComp.js
@@ -38,15 +38,15 @@ export default function SearchComp() {
     return (
         <div>
             <form onSubmit={getData}>
-                <div class="flex flex-wrap mx-3 mb-6 justify-center">
-                    <div class="w-xl px-3 flex items-baseline gap-2">
+                <div className="flex flex-wrap mx-3 mb-6 justify-center">
+                    <div className="w-xl px-3 flex items-baseline gap-2">
                         <div>
-                            <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-password"
+                            <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-password"
                                 type="text" placeholder="Search Your Movie" ref={usrInp}>
                             </input>
-                            <p class="text-gray-600 text-xs italic">Search your favorite hollywood movies and shows.</p>
+                            <p className="text-gray-600 text-xs italic">Search your favorite hollywood movies and shows.</p>
                         </div>
-                        <button class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
+                        <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
                             onClick={getData}>
                             Button
                         </button>
